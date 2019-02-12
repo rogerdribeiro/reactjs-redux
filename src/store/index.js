6197088637fb19857b2aa058a8bfb388161efb7b
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-12 17:19:06 -0200

    config sagas e axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(reducers);
+import reducers from './reducers';
+import sagas from './sagas';
+
+const middlewares = [];
+
+const sagaMiddleware = createSagaMiddleware();
+middlewares.push(sagaMiddleware);
+
+const store = createStore(reducers, compose(applyMiddleware(...middlewares)));
+
+sagaMiddleware.run(sagas);
 
 export default store;
