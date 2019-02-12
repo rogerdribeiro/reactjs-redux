5e671794ab4589d8c4aebb8cb08ffaf76bf9c79b
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-12 15:19:49 -0200

    interface -hmtl

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
 // import { Container } from './styles';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repository}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <ul>
+            <li>
+              <p>
+                <strong>Facebook/react</strong>
+                (description alkdf lkdf akldjfklasdf aksldf ajlksdf )
+              </p>
+              <a href="url">Acessar</a>
+            </li>
+          </ul>
+        </form>
+      </Fragment>
+    );
+  }
+}
