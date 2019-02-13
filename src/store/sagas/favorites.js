2c2a4ade73e20751d5ba17999905df725911c1e7
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-13 08:46:55 -0200

    proibindo repositorio duplicado

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import api from '../../services/api';
 
@@ -7,13 +7,18 @@ import { Creators as FavoritesActions } from '../ducks/favorites';
 export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
-    yield put(FavoritesActions.addFavoriteSuccess(repositoryData));
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
+    if (isDuplicated) {
+      yield put(FavoritesActions.addFavoriteFailure('Esse repositório ja existe'));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+      yield put(FavoritesActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (err) {
     yield put(FavoritesActions.addFavoriteFailure('Erro ao adicionar repositório'));
   }
