d5b4f3f76bebe239cf807e4c4c567ea8918f5616
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-13 08:25:20 -0200

    mostrando error

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,26 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 import * as FavoriteActions from '../../store/actions/favorites';
 
 class Main extends Component {
+  static propTypes = {
+    addFavoriteRequest: PropTypes.func.isRequired,
+    favorites: PropTypes.shape({
+      loading: PropTypes.bool,
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          name: PropTypes.string,
+          description: PropTypes.string,
+          url: PropTypes.string,
+        }),
+      ),
+      error: PropTypes.oneOfType([null, PropTypes.string]),
+    }).isRequired,
+  };
+
   state = {
     repositoryInput: '',
   };
@@ -27,6 +44,7 @@ class Main extends Component {
           />
           <button type="submit">Adicionar</button>
           {this.props.favorites.loading && <span>Carregando...</span>}
+          {!!this.props.favorites.error && <span>{this.props.favorites.error}</span>}
           <ul>
             {this.props.favorites.data.map(favorite => (
               <li key={favorite.id}>
--- src/store/reducers/favorites.js
+++ src/store/reducers/favorites.js
@@ -1,6 +1,7 @@
 const INITIAL_STATE = {
   loading: false,
   data: [],
+  error: null,
 };
 
 export default function favorites(state = INITIAL_STATE, action) {
@@ -8,7 +9,9 @@ export default function favorites(state = INITIAL_STATE, action) {
     case 'ADD_FAVORITE_SUCCESS':
       return { ...state, data: [...state.data, action.payload.data], loading: false };
     case 'ADD_FAVORITE_REQUEST':
-      return { ...state, loading: true };
+      return { ...state, loading: true, error: null };
+    case 'ADD_FAVORITE_FAILURE':
+      return { ...state, loading: false, error: action.payload.error };
 
     default:
       return state;
